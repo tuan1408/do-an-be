5275649b6244e3fcdbf4ed003411def8cf6da1c8
datispham <datistpham@gmail.com>
2022-05-23 19:44:40 +0700

    up

--- app.js
+++ app.js
@@ -7,33 +7,33 @@ import usersRouter from "./routes/users.js"
 import http from "http"
 import cors from "cors"
 
-const app = express();
+const app = express()
 const server= http.createServer(app)
 // view engine setup
 
-app.use(logger('dev'));
+app.use(logger('dev'))
 app.use(cors())
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
-app.use('/', indexRouter);
-app.use(usersRouter);
+app.use('/', indexRouter)
+app.use(usersRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
 server.listen(4000, ()=> console.log("Server run on port 4000"))
--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,14 @@ import express from "express"
 import { airport_autocomplete } from "../controller/airport_autocomplete.js"
 import { code_aiport } from "../controller/code_airport.js"
 import { fullsearch } from "../controller/fullsearch.js"
+import { search_hotel } from "../controller/search_hotel.js"
 import { suggest_travel } from "../controller/suggest_travel.js"
 const router = express.Router()
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource')
 })
+router.post("/api/v1/hotel/search", search_hotel)
 router.get("/api/v1/hotel/autocomplete", suggest_travel)
 router.get("/api/v1/airport/autocomplete", airport_autocomplete)
 router.get("/api/v2/airport/codeairport", code_aiport )
