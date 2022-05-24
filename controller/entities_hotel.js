b980e1eff5e7caec92416d421705c9399fa9980f
datispham <datistpham@gmail.com>
2022-05-24 23:55:47 +0700

    up

--- controller/entities_hotel.js
+++ controller/entities_hotel.js
@@ -1,4 +1,8 @@
+import connection from "../database/init.js"
+
 export const entities_hotel= (req, res)=> {
-    console.log(req.body.query_array)
-    return res.json(req.body.query_array)
+    const prepare= [req.body.query_array.wifi,req.body.query_array.pool,req.body.query_array.parking,req.body.query_array.restaurant,req.body.query_array.receptionist,req.body.query_array.elevator,req.body.query_array.fitness_center,req.body.query_array.meeting, req.body.query_array.shuttle]
+    connection.query("SELECT * FROM hotel where wifi=? and pool=? and parking=? and restaurant=? and receptionist=? and elevator=? and fitness_center=? and meeting=? and airport_shuttle=?", prepare, (err, rows, fields)=> {
+       return res.json(rows)
+    })
 }
