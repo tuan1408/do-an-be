85d7feb4e37e92679f6b6f171e097945471e70ef
datispham <datistpham@gmail.com>
2022-06-11 22:59:18 +0700

    up

--- controller/login.js
+++ controller/login.js
@@ -2,7 +2,7 @@ import connection from "../database/init.js"
 import md5 from "md5"
 
 export const login= (req, res)=> {
-    connection.query("SELECT firstname, lastname from user where account='?' and password='?'", [req.body.account, md5(req.body.password)], (err, rows, fields)=> {
+    connection.query("SELECT * from user where account=? and password=?", [req.body.account, md5(req.body.password)], (err, rows, fields)=> {
         if(err) console.log(err)
         return res.json(rows)
     })
