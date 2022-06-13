b304fcf455da9c3e1085ce451c121e4836c8722f
datispham <datistpham@gmail.com>
2022-06-13 09:12:32 +0700

    final

--- controller/search_hotel.js
+++ controller/search_hotel.js
@@ -1,7 +1,10 @@
 import connection from "../database/init.js"
 
 export const search_hotel= (req, res)=> {
-    connection.query(`SELECT * from travels where MATCH(destination) AGAINST ('${req.body.query_string}' IN NATURAL LANGUAGE MODE) OR MATCH(location_travel) AGAINST ('${req.body.query_string}' IN NATURAL LANGUAGE MODE)`, (err, rows, fields)=> {
+    // connection.query(`SELECT * from travels where MATCH(destination) AGAINST ('${req.body.query_string}' IN NATURAL LANGUAGE MODE) OR MATCH(location_travel) AGAINST ('${req.body.query_string}' IN NATURAL LANGUAGE MODE)`, (err, rows, fields)=> {
+    //     return res.json(rows)
+    // })
+    connection.query(`SELECT * from travels`, (err, rows, fields)=> {
         return res.json(rows)
     })
 }
