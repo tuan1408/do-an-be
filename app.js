fd1122f7daba7e3bfbe54a710bb3c512d250759c
tuan <tuan140899@gmail.com>
2022-06-27 21:44:01 +0700

    fix

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ app.use('/', indexRouter)
 app.use(usersRouter)
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404))
-})
+// app.use(function (req, res, next) {
+//   next(createError(404))
+// })
 
 // error handler
 app.use(function (err, req, res, next) {
