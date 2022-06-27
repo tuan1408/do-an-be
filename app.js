4acbb64a235db6a6c2fcb189ed595e7cedabf42f
tuan <tuan140899@gmail.com>
2022-06-27 21:40:11 +0700

    fix

--- app.js
+++ app.js
@@ -32,7 +32,10 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500)
-  res.render('error')
+  res.json({
+    message: err.message,
+    error: err
+  });
 })
 
 server.listen(3000, '0.0.0.0', () => console.log("Server run on port 3000"))
